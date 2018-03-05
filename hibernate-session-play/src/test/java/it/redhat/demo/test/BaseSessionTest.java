742467ef2d28605c95616a0f943731845b02480b
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2018-03-05 13:49:31 +0100

    refactoring - extracting base test class

--- hibernate-session-play/src/test/java/it/redhat/demo/test/BaseSessionTest.java
+++ hibernate-session-play/src/test/java/it/redhat/demo/test/BaseSessionTest.java
@@ -1,9 +1,4 @@
-package it.redhat.demo.test;/*
- * Hibernate OGM, Domain model persistence for NoSQL datastores
- *
- * License: GNU Lesser General Public License (LGPL), version 2.1 or later
- * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
- */
+package it.redhat.demo.test;
 
 import java.util.function.Consumer;
 
