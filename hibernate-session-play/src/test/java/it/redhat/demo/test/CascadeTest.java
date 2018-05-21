b9eaa9fe63b9f75ec0e57a742b4d13c7c469e6d5
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2018-05-21 16:06:07 +0200

    Collection Persister | Loader tests

--- hibernate-session-play/src/test/java/it/redhat/demo/test/CascadeTest.java
+++ hibernate-session-play/src/test/java/it/redhat/demo/test/CascadeTest.java
@@ -2,6 +2,7 @@ package it.redhat.demo.test;
 
 import static org.junit.Assert.assertEquals;
 
+
 import java.time.LocalDateTime;
 
 import org.junit.Rule;
@@ -19,7 +20,52 @@ public class CascadeTest extends BaseSessionTest {
 	public ExpectedException thrown = ExpectedException.none();
 
 	@Test
-	public void test_cascade_merge() {
+	public void detachUpdateMerge() {
+		inTransaction( session -> {
+			Party party = new Party( 1, ARTIST_1, LocalDateTime.of( 2018, 12, 21, 22, 30, 0 ), "Budapest" );
+			session.persist( party );
+		} );
+
+		inTransaction( session -> {
+			Party party = session.load( Party.class, 1 );
+			assertEquals( ARTIST_1, party.getName() );
+
+			// detach - update - merge
+			session.detach( party );
+			party.setName( ARTIST_2 );
+			session.merge( party );
+		} );
+
+		inTransaction( session -> {
+			Party party = session.load( Party.class, 1 );
+			assertEquals( ARTIST_2, party.getName() );
+		} );
+	}
+
+	@Test
+	public void updateAttachedRefresh() {
+		inTransaction( session -> {
+			Party party = new Party( 2, ARTIST_1, LocalDateTime.of( 2018, 12, 21, 22, 30, 0 ), "Budapest" );
+			session.persist( party );
+		} );
+
+		inTransaction( session -> {
+			Party party = session.load( Party.class, 2 );
+			assertEquals( ARTIST_1, party.getName() );
+
+			// update attached - refresh
+			party.setName( ARTIST_2 );
+			session.refresh( party );
+		} );
+
+		inTransaction( session -> {
+			Party party = session.load( Party.class, 2 );
+			assertEquals( ARTIST_1, party.getName() );
+		} );
+	}
+
+	@Test
+	public void detachUpdateMergeOnCascade() {
 		String festivalName = "Sziget 2018";
 		inTransaction( session -> {
 			Party party = new Party( 4, ARTIST_1, LocalDateTime.of( 2018, 12, 21, 22, 30, 0 ), "Budapest" );
@@ -31,8 +77,10 @@ public class CascadeTest extends BaseSessionTest {
 		inTransaction( session -> {
 			Festival festival = session.load( Festival.class, festivalName );
 			Party party = festival.getParties().get( 0 );
-
 			assertEquals( ARTIST_1, party.getName() );
+
+			// detach parent - update - merge parent
+			session.detach( festival );
 			party.setName( ARTIST_2 );
 			session.merge( festival );
 		} );
@@ -45,10 +93,10 @@ public class CascadeTest extends BaseSessionTest {
 	}
 
 	@Test
-	public void test_cascade_refresh() {
+	public void updateAttachedRefreshOnCascade() {
 		String festivalName = "Sziget 2019";
 		inTransaction( session -> {
-			Party party = new Party( 5, ARTIST_1, LocalDateTime.of( 2019, 12, 21, 22, 30, 0 ), "Budapest" );
+			Party party = new Party( 5, ARTIST_1, LocalDateTime.of( 2018, 12, 21, 22, 30, 0 ), "Budapest" );
 			Festival festival = new Festival( festivalName );
 			festival.add( party );
 			session.persist( festival );
@@ -58,6 +106,7 @@ public class CascadeTest extends BaseSessionTest {
 			Festival festival = session.load( Festival.class, festivalName );
 			Party party = festival.getParties().get( 0 );
 
+			// update attached - refresh parent
 			assertEquals( ARTIST_1, party.getName() );
 			party.setName( ARTIST_2 );
 			session.refresh( festival );
