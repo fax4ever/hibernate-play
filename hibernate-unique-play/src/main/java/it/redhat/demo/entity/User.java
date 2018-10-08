22ac58ef080053af7b5c5795214464f218e1bb35
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2018-10-08 13:22:13 +0200

    Add test project: unique marker sql schema

---
+++ hibernate-unique-play/src/main/java/it/redhat/demo/entity/User.java
@@ -0,0 +1,85 @@
+package it.redhat.demo.entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "User_", uniqueConstraints = @UniqueConstraint(columnNames = { "client_id", "name" }))
+public class User implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(nullable = false, length = 255)
+	private String name;
+
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name = "client_id", nullable = false)
+	private Client client;
+
+	@Column(name = "credentials_id", nullable = false, unique = true)
+	private long credentialsId;
+
+	@Column(name = "functionrights_id", nullable = false, unique = true)
+	private long functionRightsId;
+
+	public User() {
+	}
+
+	public User(String name, long credentialsId, long functionRightsId) {
+		this.name = name;
+		this.credentialsId = credentialsId;
+		this.functionRightsId = functionRightsId;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public long getCredentialsId() {
+		return credentialsId;
+	}
+
+	public void setCredentialsId(long credentialsId) {
+		this.credentialsId = credentialsId;
+	}
+
+	public long getFunctionRightsId() {
+		return functionRightsId;
+	}
+
+	public void setFunctionRightsId(long functionRightsId) {
+		this.functionRightsId = functionRightsId;
+	}
+}
---
+++ hibernate-unique-play/src/test/java/it.redhat.demo.test/UniqueCreateTest.java
@@ -0,0 +1,76 @@
+package it.redhat.demo.test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import it.redhat.demo.entity.Client;
+import it.redhat.demo.entity.User;
+
+public class UniqueCreateTest {
+
+	private static final String USER_NAME = "Am I?";
+	private static final String CLIENT_NAME = "My name is";
+
+	private SessionFactory factory;
+
+	private Client client;
+	private User user;
+
+	@Before
+	public void setUp() {
+		client = new Client( CLIENT_NAME );
+		user = new User( USER_NAME, 7l, 39l );
+		user.setClient( client );
+
+		Configuration config = new Configuration()
+				.addAnnotatedClass( Client.class )
+				.addAnnotatedClass( User.class );
+
+		factory = config.buildSessionFactory( new StandardServiceRegistryBuilder().build() );
+		try ( Session session = factory.openSession() ) {
+			Transaction transaction = session.beginTransaction();
+			session.persist( client );
+			session.persist( user );
+			transaction.commit();
+		}
+	}
+
+	@After
+	public void tearDown() {
+		if ( factory == null ) {
+			return;
+		}
+
+		try ( Session session = factory.openSession() ) {
+			Transaction transaction = session.beginTransaction();
+			session.remove( user );
+			session.remove( client );
+			transaction.commit();
+		}
+		finally {
+			factory.close();
+		}
+	}
+
+	@Test
+	public void test() {
+		try ( Session session = factory.openSession() ) {
+			List<User> userByClientName = session.createQuery( "select u from User u where u.client.name = :name" )
+					.setParameter( "name", CLIENT_NAME )
+					.getResultList();
+
+			assertThat( userByClientName.get( 0 ).getName() ).isEqualTo( USER_NAME );
+		}
+	}
+}
