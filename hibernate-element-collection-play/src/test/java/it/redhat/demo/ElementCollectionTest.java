36cef5e1ede620c0e3dfb34a576fd125b2f5e748
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2018-10-19 12:09:37 +0200

    Add element collection with duplicates case

--- hibernate-element-collection-play/src/test/java/it/redhat/demo/ElementCollectionTest.java
+++ hibernate-element-collection-play/src/test/java/it/redhat/demo/ElementCollectionTest.java
@@ -14,7 +14,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class UniqueCreateTest {
+public class ElementCollectionTest {
 
 	public static final String NAME = "Office";
 	public static final String ADDRESS = "via Andrea Doria, 41M 00192 Rome Italy";
